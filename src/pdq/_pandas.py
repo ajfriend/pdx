1ff5345a82b4ff3a1120e2bbec1975117e3b93fa
AJ Friend <ajfriend@gmail.com>
2022-09-26 10:10:45 -0700

    i like it this way better

--- src/pdq/_pandas.py
+++ src/pdq/_pandas.py
@@ -1,4 +1,4 @@
-import pandas
+from pandas.core.base import PandasObject
 
 from . import _query
 
@@ -11,64 +11,61 @@ def _insist_single_col(df):
         raise ValueError(f'DataFrame has {len(df.columns)} columns, but should only have 1.')
 
 
-@pandas.api.extensions.register_dataframe_accessor('pdq')
-class PDQAccessor:
-    def __init__(self, pandas_obj):
-        self._df = pandas_obj
 
-    def sql(self, s='', tbl_name='tbl'):
-        """ Run a SQL query against Pandas DataFrame.
+def _sql(df, s='', tbl_name='tbl'):
+    """ Run a SQL query against Pandas DataFrame.
 
-        DataFrame will be referred to by string given in `tbl_name`.
+    DataFrame will be referred to by string given in `tbl_name`.
 
-        Examples
-        --------
-        >>> df.sql('select * from tbl')
-        >>> df.sql('select * from new_tbl', tbl_name='new_tbl')
+    Examples
+    --------
+    >>> df.sql('select * from tbl')
+    >>> df.sql('select * from new_tbl', tbl_name='new_tbl')
 
-        """
-        if not s:
-            s = f'select * from {tbl_name}'
+    """
+    if not s:
+        s = f'select * from {tbl_name}'
 
-        tables = {tbl_name: self._df}
+    tables = {tbl_name: df}
 
-        return _query.sql(s, **tables)
+    return _query.sql(s, **tables)
 
-    def prql(self, s='', tbl_name='tbl'):
-        s = f'from {tbl_name}\n' + s
-        tables = {tbl_name: self._df}
+def _prql(df, s='', tbl_name='tbl'):
+    s = f'from {tbl_name}\n' + s
+    tables = {tbl_name: df}
 
-        return _query.prql(s, **tables)
+    return _query.prql(s, **tables)
 
-    def as_list(self):
-        """Transform a df with one column to a list"""
-        df = self._df
-        _insist_single_col(df)
+def _as_list(df):
+    """Transform a df with one column to a list"""
+    _insist_single_col(df)
 
-        col = df.columns[0]
-        out = list(df[col])
+    col = df.columns[0]
+    out = list(df[col])
 
-        return out
+    return out
 
-    def as_dict(self):
-        """Transform a df with one row to a dict
-        """
-        df = self._df
-        _insist_single_row(df)
+def _as_dict(df):
+    """Transform a df with one row to a dict
+    """
+    _insist_single_row(df)
 
-        out = dict(df.iloc[0])
+    out = dict(df.iloc[0])
 
-        return out
+    return out
 
-    def as_item(self):
-        """Transform a df with one row and one column to single element"""
-        df = self._df
-        _insist_single_row(df)
-        _insist_single_col(df)
+def _as_item(df):
+    """Transform a df with one row and one column to single element"""
+    _insist_single_row(df)
+    _insist_single_col(df)
 
-        col = df.columns[0]
-        out = list(df[col])
-        out = out[0]
+    out = _as_list(df)[0]
 
-        return out
+    return out
 
+
+PandasObject.sql = _sql
+PandasObject.prql = _prql
+PandasObject.as_list = _as_list
+PandasObject.as_dict = _as_dict
+PandasObject.as_item = _as_item
