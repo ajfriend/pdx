016c98bfa11c717a01004bdf14aa7c517ef3bb47
AJ Friend <ajfriend@gmail.com>
2023-06-25 18:16:11 -0700

    Update __init__.py

--- src/pdx/__init__.py
+++ src/pdx/__init__.py
@@ -6,12 +6,15 @@ from ._query import (
 
 from . import _pandas # triggers pandas monkeypatching
 
-try:
+try: # dumb hack to get this to work on 3.7 today
     from . import _data as data
 except:
     pass
     
 from ._io import save, load
 
-import importlib.metadata
-__version__ = importlib.metadata.version(__package__ or __name__)
+try: # dumb hack to get this to work on 3.7 today
+    import importlib.metadata
+    __version__ = importlib.metadata.version(__package__ or __name__)
+except:
+    __version__ = '0.4.0'
